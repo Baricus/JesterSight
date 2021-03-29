b0e66472f427095cb930c0d35043e5ebf70f3e0a
Miles Shamo <Baricus>
2021-03-29 13:12:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from mss import mss
 import time
-import cv2
-import numpy
+import cv2 as cv
+import numpy as np
+
+def detect(img1, img2):
+    
+
 
 # sets monitor positions and initializes mss
 monitor = {"top": 0, "left": 0, "width": 160, "height": 135}
 sct = mss()
 
 # loads in reference true crown
-
+crown = np.array(cv.imread(cv.samples.findFile('crown.png'), cv.IMREAD_GRAYSCALE))
 
 frames = 0
 avgFPS = 0
@@ -17,12 +21,10 @@ while True:
     a = time.time()
 
     # grabs image and converts to greyscale for processing
-    img = numpy.array(sct.grab(monitor))
-    grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        
-
-    cv2.imshow('Gray image', grey)
-    cv2.waitKey(1)
+    img = np.array(sct.grab(monitor))
+    grey = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    
+    detect(crown, grey)
     
     b = time.time()
     avgFPS = (avgFPS * frames) + (1 / (b - a))
