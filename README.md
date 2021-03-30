75d4f0b8e3e771bf0763cf6fe2ab52d13ec41a4a
Miles Shamo <Baricus>
2021-03-30 00:10:40 -0500

    Basic working system
    using color matching gives us a reliable true crown indicator, though this needs further testing.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # JesterSight
  A Spelunky 2 True Crown timer that does not require game modification.
+
+This is currently largely a proof of concept; it is barely functional and still has bugs that need to be worked out.  The main one is that Olmec (2nd phase largely) can be considered the true crown causing the system to trigger.   
--- main.py
+++ main.py
@@ -1,36 +1,76 @@
 from mss import mss
+from apscheduler.schedulers.background import BackgroundScheduler
 import time
 import cv2 as cv
 import numpy as np
 
-def detect(img1, img2):
+def detect(img):
+    #converts to HSV
+    hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+
+    # masks against both yellow and red
+    lowerY = np.array([20, 80, 100])
+    upperY = np.array([30, 200, 200])
+    mYellow = cv.inRange(hsv_img, lowerY, upperY)
+
+    lowerR = np.array([0, 150, 120])
+    upperR = np.array([20, 220, 200])
+    mRed = cv.inRange(hsv_img, lowerR, upperR)
+
+    # combines for a true crown mask
+    mTot = mRed+mYellow
+
+    # takes mean of pixels (we only care about channel 0)
+    channels = cv.mean(mTot)
+
+    # prints the mask and original image (debug)
+    #print(channels)
+    cv.imshow('Matches', mTot)
+    #cv.imshow('orig', img)
+    cv.waitKey(1)
     
+    if (channels[0] > 70):
+        return True
+    return False
+
+    
+def beep():
+    print("BEEP")
 
 
 # sets monitor positions and initializes mss
-monitor = {"top": 0, "left": 0, "width": 160, "height": 135}
+monitor = {"top": 8, "left": 40, "width": 65, "height": 27}
 sct = mss()
 
-# loads in reference true crown
-crown = np.array(cv.imread(cv.samples.findFile('crown.png'), cv.IMREAD_GRAYSCALE))
+# sets up the scheduler for the timer
+scheduler = BackgroundScheduler()
+beepJob = scheduler.add_job(beep, 'interval', seconds=1, id='main_beep')
+beepJob.pause()
+isRunning = False;
+scheduler.start()
 
-frames = 0
-avgFPS = 0
+# counter for max misses
+counter = 10
 
 while True:
     a = time.time()
 
     # grabs image and converts to greyscale for processing
     img = np.array(sct.grab(monitor))
-    grey = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    
-    detect(crown, grey)
-    
-    b = time.time()
-    avgFPS = (avgFPS * frames) + (1 / (b - a))
-    frames += 1
-    avgFPS = avgFPS / frames
-    print("fps: {}".format(avgFPS))
+    #grey = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
+    # if we don't find the image 10 "frames" in a row, about 1/10 of a sec,
+    # stop the timer
+    if detect(img):
+        counter = 10
+    else:
+        counter -= 1
 
-
+    if counter <= 0:
+        counter = 0
+        beepJob.pause()
+        isRunning = False
+    else:
+        if not isRunning:
+            beepJob.resume()
+            isRunning = True
