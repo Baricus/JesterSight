52981e05d96315881e324035f29c77ef6b715e46
Ansel Rosso Klakovich <rossoklakovich@gmail.com>
2021-11-14 21:48:54 +0000

    comments

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import cv2
 import mss
 import pytesseract
 
-
+# detect the center slash of the timer(detects as a 7)
 def parse_string(string):
     mid = string.find('70')
     if mid == -1:
+        # return a -1 if not found
         return -1
     else:
+        # return the two numbers before the middle
         return string[mid - 2:mid]
 
-
+# get a screenshot of the top left corner of the screen
 def get_top_left():
     sct = mss.mss()
     monitor_left = {'top': 0, 'left': 0, 'width': 300, 'height': 100}
@@ -20,7 +22,7 @@ def get_top_left():
     cv2.imshow('image', img_left)
     cv2.waitKey(10)
 
-
+# get a screenshot of the top right corner of the screen
 def get_top_right():
     sct = mss.mss()
     monitor_right = {'top': 50, 'left': 1920 - 405, 'width': 250, 'height': 50}
@@ -28,6 +30,7 @@ def get_top_right():
     img_right = np.array(screen_right)
     cv2.imshow('image', img_right)
     cv2.waitKey(10)
+    # detect text in the image using pytesseract
     custom_config = r'--oem 3 --psm 6 outputbase digits'
     print(parse_string(pytesseract.image_to_string(img_right, config=custom_config)))
 
